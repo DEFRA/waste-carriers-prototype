123af6b7252e38305cb6a898ecd258681e87eaba
christinagyles_ea <christina.gyles@environment-agency.gov.uk>
2020-06-12 11:27:47 +0100

    start page and new registration page

--- app/routes_registration_v1.js
+++ app/routes_registration_v1.js
@@ -3,6 +3,22 @@ const router = express.Router()
 
 var folder = "registration-v1"
 
+// HTML for standard buttons
+var backlink = '<a href="javascript:history.back()" class="govuk-back-link">Back</a>'
 
-
+router.use(function (req, res, next) {
+    // set a folder and store in locals
+    // this can then be used in pages as {{folder}}
+    res.locals.folder=folder
+    res.locals.backlink=backlink
+    res.locals.submitButton=submitButton
+    res.locals.completeLink=completeLink
+    res.locals.paymentMethod=paymentMethod
+    res.locals.serviceName=servicename
+    // permit and autostore data set in all statement at bottom
+    res.locals.permit=res.locals.data
+  
+    next()
+  });
+  
 module.exports = router
